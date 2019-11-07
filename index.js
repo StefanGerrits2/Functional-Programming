59232f0678373c6e82c821188ca8fbda7cc27aa6
Stefan Gerrits <stefangerrits2000@gmail.com>
2019-11-07 13:15:25 +0100

    add async and clean data

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
 const query = `
-    PREFIX dc: <http://purl.org/dc/elements/1.1/>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX dct: <http://purl.org/dc/terms/>
-    PREFIX edm: <http://www.europeana.eu/schemas/edm/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT 
-        ?obj
-        ?secondLevel
-        ?title
-        ?continentLabel
-        ?catName
-        ?subCatName
-        ?subSubCatName
-        ?subSubSubCatName
-        ?type
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+    SELECT ?categoryName (COUNT(?category) AS ?categoryAmount) 
 
     WHERE {
-        <https://hdl.handle.net/20.500.11840/termmaster2704> skos:* ?cat .
-        ?obj edm:isRelatedTo ?cat .
-        ?cat skos:narrower ?subCat .
-        ?subCat	skos:narrower ?subSubCat .
-        ?subSubCat skos:narrower ?subSubSubCat .
-
-        ?obj dc:title ?title .
-        ?obj dct:spatial ?origin .
-        ?origin skos:broader ?continent .
-        ?continent skos:prefLabel ?continentLabel .
-        ?obj dc:type ?type .
-        
-        ?cat skos:prefLabel ?catName .
-        ?subCat skos:prefLabel ?subCatName .
-        ?subSubCat skos:prefLabel ?subSubCatName .
-        ?subSubSubCat skos:prefLabel ?subSubSubCatName .
-
-
-    } LIMIT 100
+    <https://hdl.handle.net/20.500.11840/termmaster2704> skos:narrower* ?category .
+    ?category skos:prefLabel ?categoryName .
+    ?obj edm:isRelatedTo ?category .
+    
+    } LIMIT 200
     `
 const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-03/sparql";
 
-runQuery(url, query);
-function runQuery(url, query){
-    fetch(url+'?query='+ encodeURIComponent(query) +'&format=json') // Get data from API with my query
-    .then(res => res.json()) // Data is being send back as JSON 
-    .then(json => {
-        results = json.results.bindings; // Save all objects in results
-        console.log(results)
-    });
+/* IIFI */
+(async () => {
+    let myResults = await runQuery(url, query);
+    console.log(cleanData(myResults))
+})();
+
+async function runQuery(url, query){
+    let response = await fetch(url+'?query='+ encodeURIComponent(query) +'&format=json');
+    let json = await response.json();
+    return json.results.bindings;
+}
+
+function cleanData(results) {
+    return results.reduce((cleanResults, results) => {
+        let pairs = [];
+        for(let key in results) {
+            let string = results[key].value;
+            let stringWithoutFirsLetter = string.slice(0);
+            let upperCased = string.charAt(0).toUpperCase() + stringWithoutFirsLetter.slice(1);
+            cleanResults.push(upperCased);
+        }
+        return cleanResults;
+    },[])
 } 
