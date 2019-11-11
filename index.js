9a88d9a6cc09cb94d7924482492ed18c3b4c52fe
Stefan Gerrits <stefangerrits2000@gmail.com>
2019-11-11 20:15:12 +0100

    change query add category function

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const query = `
     PREFIX edm: <http://www.europeana.eu/schemas/edm/>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-    SELECT ?categoryName (COUNT(?category) AS ?categoryAmount) 
+    SELECT ?categoryName (COUNT(?category) AS ?categoryAmount) ?upperCategory
 
     WHERE {
     <https://hdl.handle.net/20.500.11840/termmaster2704> skos:narrower* ?category .
     ?category skos:prefLabel ?categoryName .
     ?obj edm:isRelatedTo ?category .
-    
-    } LIMIT 100
+    ?category skos:broader ?categoryGroup .
+    ?categoryGroup skos:prefLabel ?upperCategory .
+    } 
+    LIMIT 100
     `
 const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-03/sparql";
 
 runQuery(url, query) // Run main function
-    .then((myRawResults) => console.log(cleanData(myRawResults))) // When data is obtained, pass data into a new function
-    .then()
+    .then((myRawResults) => (cleanData(myRawResults))) // When data is obtained, pass data into a new function
+    .then((data) => console.log(fixCategory(data)))
 
 async function runQuery(url, query){
     let response = await fetch(url+'?query='+ encodeURIComponent(query) +'&format=json'); // Fetch data into response
@@ -30,6 +32,7 @@ async function runQuery(url, query){
 function cleanData(results) {
     for(let key in results) { // Loop over every value
         results[key].categoryName = results[key].categoryName.value.charAt(0).toUpperCase() + results[key].categoryName.value.slice(1); // Change first character to uppercase + full string except first character
+        results[key].upperCategory = results[key].upperCategory.value.charAt(0).toUpperCase() + results[key].upperCategory.value.slice(1); // Change first character to uppercase + full string except first character
         results[key].categoryAmount = parseInt(results[key].categoryAmount.value); // The number in every string will be converted into a number
         // Delete unneeded properties from object
         delete results[key].categoryAmount.type;
@@ -37,4 +40,15 @@ function cleanData(results) {
         delete results[key].categoryName.type;
     }
     return results; // Return results
+}
+
+function fixCategory(data) {
+    // Loop over every value
+    for(let key in data) { 
+        // Fix top category
+        if (data[key].upperCategory.includes("Functionele categorie")) {
+            data[key].upperCategory = data[key].categoryName;
+        }
+    }
+    return data
 }
